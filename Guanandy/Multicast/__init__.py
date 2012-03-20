b9321c2dd7739aeba564c797ba7ccb8c0991b706
Wiliam Souza <wiliam@mandriva.com>
2012-03-20 20:32:53 -0300

    Changed multicast server working mechanism

--- Guanandy/Multicast/__init__.py
+++ Guanandy/Multicast/__init__.py
@@ -1,20 +1,37 @@
+"""
+Multicast server and client that use OpenPGM.
+
+"""
+
 import os
+import time
 import hashlib
+import logging
 
 import zmq
 
 from PySide import QtCore
 
-class MulticastServer(QtCore.QThread):
+logger = logging.getLogger(__name__)
 
-    def __init__(self, filePath, ip, port=5555, parent=None):
+
+class MulticastServer(QtCore.QThread):
+    """
+    Multicast server
+    """
+
+    def __init__(self, ip, port, parent):
         QtCore.QThread.__init__(self, parent)
         self.running = False
-        self.endpoint = 'epgm://%s;239.192.0.1:%s' % (ip, port)
-        self.filePath = filePath
-        self.fileSize = os.path.getsize(self.ilePath)
+        self.context = None
+        self.message = None
+        self.publisher = None
+        self.filePath = None
+        self.fileSize = None
+        self.uri = 'epgm://%s;239.192.0.1:%s' % (ip, port)
 
     def run(self):
+        logger.info('Starting multicast server on: %s' % self.uri)
         self.running = True
         self.context = zmq.Context()
         self.publisher = self.context.socket(zmq.PUB)
@@ -25,32 +42,45 @@ class MulticastServer(QtCore.QThread):
         # Discard unsent messages on close
         #self.publisher.setsockopt(zmq.LINGER, 0)
 
-        self.publisher.bind(self.endpoint)
+        try:
+            self.publisher.bind(self.uri)
+        except:
+            logger.critical('Multicast server bind error: {0}'.format(sys.exc_info()[1]))
+            self.stop()
 
-        self.f = open(file_path, 'rb')
+        #self.f = open(file_path, 'rb')
+        #self.md5 = hashlib.md5()
 
-        self.md5 = hashlib.md5()
-
-        self.total = 0
+        #self.total = 0
         while self.running:
-            data = self.f.read(1436)
-            self.md5.update(data)
-            self.publisher.send(data)
-            total += 1436
+            if self.message:
+                #data = self.f.read(1436)
+                #self.md5.update(data)
+                self.publisher.send(self.message)
+                logger.debug('Multicast server sent a message: %s' % self.message)
+                self.message = None
+                #total += 1436
 
-            if not data:
-                self.stop()
+                #if not data:
+                #    self.stop()
+            time.sleep(1)
+
+    def send(message):
+        self.message = message
 
     def stop(self):
         self.running = False
-        self.f.close()
-        print 'md5 checksum:', md5.hexdigest()
+        #self.f.close()
+        #print 'md5 checksum:', md5.hexdigest()
         self.wait()
         self.exit()
 
 class MulticastClient(QtCore.QThread):
+    """
+    Multicast client
+    """
 
-    def __init__(self, filePath, ip, port=5555, parent=None):
+    def __init__(self, filePath, ip, port, parent):
         QtCore.QThread.__init__(self, parent)
         self.running = False
         self.endpoint = 'epgm://%s;239.192.0.1:%s' % (ip, port)
